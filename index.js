9c83a694acfc7902a8c25edc6dde7c7a315fbd0b
Clara Pons <clarapons@MacBook-Air-de-Clara.local>
2022-06-23 11:56:52 +0200

    last commit

--- index.js
+++ index.js
@@ -9,18 +9,18 @@
 // console.log(ingredients.indexOf("butter"))
 
 // 03 - Add and Remove
-// const objects = ["pen","book", "lamp"]
-// objects.unshift("chair")
-// console.log(objects)
+// const things = ["pen","book", "lamp"]
+// things.unshift("chair")
+// console.log(things)
 
-// objects.pop()
-// console.log(objects)
+// things.pop()
+// console.log(things)
 
-// objects.push("laptop")
-// console.log(objects)
+// things.push("laptop")
+// console.log(things)
 
-// objects.shift()
-// console.log(objects)
+// things.shift()
+// console.log(things)
 
 // 04 - Order
 // const numbers = [4,10,8,12,6]
@@ -33,32 +33,30 @@
 // let total = 0
 // for (let i = 0; i <= limit; i++) {
 //     total += i
-//     console.log(total)
 // }
+
+//     console.log(total)
 
 // 06 - Reverse
 
 // const sentence = "Hello Konexio !"
-// const sentenceArray = sentence.split("")
-// console.log(sentenceArray)
-// sentenceArrayBis = []
+// let sentenceReverse = ""
 
-// for(i = 0; i < sentenceArray.length; i++){
-//     console.log(i)
-//     sentenceArrayBis.unshift(sentenceArray[i])
-// }
+// for(i = sentence.length - 1; i >= 0; i--){
+//     sentenceReverse = sentenceReverse + sentence.charAt(i))
+//  }
+//     console.log(sentenceReverse)
 
-// console.log(sentenceArrayBis.join(''))
 
 // 07 - Fizzbuzz
 // for (let i = 0; i < 100; i++){
-//     if(i % 7 === 0 ){
-//         console.log("Fizz")
+//     if(i % 7 !== 0 ){
 //     }else if(i % 3 === 0 && i % 5 === 0){
-//         console.log("Buzz")
-//     }else if(i % 5 === 0){
 //         console.log("fizzbuzz")
+//     }else if(i % 5 === 0){
+//         console.log("Buzz")
 //     }else if (i % 3 === 0){
+//        console.log("Fizz")
 //     }else{
 //         console.log(i)
 //     }
@@ -69,36 +67,41 @@
 // let total = 0
 // let i = 0
 
-// while (i < limit){
+// while (i <= limit){
+//     total += i
 //     i++
-//     console.log(total += i)
 // }
+
+// console.log(total)
 
 //  09 - Sans boucle
 // const students = ["Dahn", "Arthur", "Wallid", "Edaly", "Lynda", "Jade"]
 // const min = 0
-// const max = 5
+// const max = students.length 
 
 // const random = Math.floor(Math.random() * max) + min
 // console.log(students[random])
 
 // // 10 - Random and max
-// const array = []
-// const max = 100
+// let numbers = []
+// const max = 101
 // const min = 0
 
 // for(let i = 0; i < 20; i++){
 //     let random = Math.round(Math.random() * (max)) + min
-//     array.push(random)
+//     numbers.push(random)
 // }
 
-// console.log(array)
-// let maxArray = array[0]
+// console.log(numbers)
+// let maxArray = 0
 
 // for(let i = 0; i < 20; i++){
-//     if (array[i] > maxArray){
-//         maxArray = array[i]  
+//     if (numbers[i] > maxArray){ // si 73 > 0
+//         maxArray = numbers[i]  // max = 73 etc 
 //     }
 // }
 
 // console.log(maxArray)
+
+
+
